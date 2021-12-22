498128a2ba20812cfb5160743706a8c8b35e42ba
Gorka <iibargurenca20dw@ikzubirimanteo.com>
2021-12-22 10:07:56 +0100

    Vista Formu Login terminado(falta quitar fondo blanco)

--- resources/views/landingPage/index.blade.php
+++ resources/views/landingPage/index.blade.php
@@ -11,18 +11,17 @@
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
-        rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
     <!--Jquery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Core theme CSS (includes Bootstrap)-->
-    
+
     <link href="{{ asset('landing/css/styles.css') }}" rel="stylesheet" />
     <!-- Personal CSS-->
     <link href="{{ asset('landing/css/main.css') }}" rel="stylesheet" />
+    <link href="{{ asset('landing/css/login.css') }}" rel="stylesheet" />
 </head>
 
 <body id="page-top">
@@ -33,21 +32,17 @@
                 <img src="{{ asset('landing/assets/img/logo_blanco.png') }}" alt="" width="50" class="d-inline-block ">
                 Money Manager
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="#signup"
-                            href="#!">Registrate</a></li>
-                    <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="#login"
-                            href="#!">Acceder</a></li>
+                    <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="#signup" href="#!">Registrate</a></li>
+                    <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="#login" href="#!">Acceder</a></li>
                 </ul>
             </div>
         </div>
     </nav>
     <!-- Login-->
-    <div class="modal fade mt-5" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- <div class="modal fade mt-5" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -71,7 +66,42 @@
                 </div>
             </div>
         </div>
+    </div> -->
+    <div class="modal fade mt-5" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="form-bg">
+                    <div class="container">
+                        <div class="row bg-transparent">
+                            <div class="" style="width:100%;">
+                                <div class="form-container">
+                                    <div class="form-icon">
+                                        <img src="{{asset('landing/assets/logo_blanco.ico')}}" width="100px">
+                                    </div>
+                                    <form class="form-horizontal">
+                                        <h3 class="title">Login</h3>
+                                        <div class="form-group">
+                                            <span class="input-icon"><i class="fa fa-envelope"></i></span>
+                                            <input class="form-control" type="email" placeholder="Correo Electrónico">
+                                        </div>
+                                        <div class="form-group">
+                                            <span class="input-icon"><i class="fa fa-lock"></i></span>
+                                            <input class="form-control" type="password" placeholder="Contraseña">
+                                        </div>
+                                        <button class="btn signin">Login</button>
+                                        <span class="forgot-pass"><a href="#">Forgot Username/Password?</a></span>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+
     <!-- Sign up-->
     <div class="modal fade mt-5" id="signup" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -139,11 +169,9 @@
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-lg-2 ">
                     <div class="p-5 hero-animation-img">
-                        <img class="img-fluid-2 d-none d-lg-block animation-two" src="{{ asset('landing/assets/img/hero-animation-01.svg') }}"
-                            alt="..." />
+                        <img class="img-fluid-2 d-none d-lg-block animation-two" src="{{ asset('landing/assets/img/hero-animation-01.svg') }}" alt="..." />
                         <img class="img-fluid  animation-one" src="{{ asset('landing/assets/img/hero-single-img-1.svg') }}" alt="..." />
-                        <img class="img-fluid-2 d-none d-lg-block animation-four" src="{{ asset('landing/assets/img/hero-animation-03.svg') }}"
-                            alt="..." />
+                        <img class="img-fluid-2 d-none d-lg-block animation-four" src="{{ asset('landing/assets/img/hero-animation-03.svg') }}" alt="..." />
                     </div>
                 </div>
                 <div class="col-lg-6 order-lg-1">
@@ -207,15 +235,13 @@
                                     <input class="form-control" id="name" type="text" placeholder="Nombre" />
                                 </div>
                                 <div class="col-6">
-                                    <input class="form-control" id="emailAddress" type="email"
-                                        placeholder="Correo Electronico" />
+                                    <input class="form-control" id="emailAddress" type="email" placeholder="Correo Electronico" />
                                 </div>
                             </div>
                         </div>
                         <!-- Message input -->
                         <div class="mb-3">
-                            <textarea class="form-control" id="message" type="text" placeholder="Mensage"
-                                style="height: 10rem;"></textarea>
+                            <textarea class="form-control" id="message" type="text" placeholder="Mensage" style="height: 10rem;"></textarea>
                         </div>
                         <!-- Form submit button -->
                         <div class="d-grid">
