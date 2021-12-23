45156c8a0c1cfded9b26746c3ab56f2624fb38eb
Hodei <hmaidaganur20dw@ikzubirimanteo.com>
2021-12-23 10:40:33 +0100

    Comprobacion de loggin terminada

--- public/landing/js/scripts.js
+++ public/landing/js/scripts.js
@@ -1,22 +1,36 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.4 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-// $(window).scroll(function () {
-//     if ($("#nav1").offset().top > 500) {
-//         $("#nav1").css("background-color","rgba(255, 102, 102,1)");
-//     } else {
-//         $("#nav1").css("background-color","rgba(0, 0, 0,0.8)");
-//     }
-// });
-$('document').ready(function(){
+$('document').ready(function () {
     let height = $(window).height();
     height = height - 311.59;
-    let top = height/2;
-    let bottom = height/2;
-    $('.masthead').css({'padding-top':top,'padding-bottom':bottom});
+    let top = height / 2;
+    let bottom = height / 2;
+    $('.masthead').css({ 'padding-top': top, 'padding-bottom': bottom });
+    $('#enviar').click(validarLogin);
 });
+
+
+function validarLogin(event) {
+    var email = $('#emailLogin').val();
+    var password = $('#passwordLogin').val();
+    var falso = true;
+    if (email == "" || !checkEmail()) {
+        $('#padreEmailLogin').css("border", "1px solid #cb0104");
+        falso = false;
+    } else {
+        $('#padreEmailLogin').css("border", "1px solid #b5b5b5");
+    }
+    if (password == "") {
+        $('#padrePasswordLogin').css("border", "1px solid #cb0104");
+        falso = false;
+    } else {
+        $('#padrePasswordLogin').css("border", "1px solid #b5b5b5");
+    }
+    if (!falso) {
+        event.preventDefault();
+        return null;
+    }
+    
+}
+function checkEmail() {
+    var regex = /[a-zA-Z0-9_\-\.\+]+\@[a-zA-Z0-9-]+\.[a-zA-Z]+/;
+    return regex.test($('#emailLogin').val());
+}
