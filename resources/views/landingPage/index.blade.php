217b9e12196eb785014fff6b3a9730a5adc7d27e
jon labayen <hmaidaganur20dw@ikzubirimanteo.com>
2022-01-13 11:29:34 +0100

    Cambio cuando inicia sesion no esneña registro

--- resources/views/landingPage/index.blade.php
+++ resources/views/landingPage/index.blade.php
@@ -35,7 +35,12 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
+                    @auth
+                    
+                    @else
                     <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="/signup" href="/signup/create">Registrate</a></li>
+                    @endauth
+
                     @auth
                     <li class="nav-item"><a class="nav-link" href="/movements">Mis Cuentas</a></li>
                     @else
