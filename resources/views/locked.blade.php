73556946a22e3349afb1fdb547254c677539720b
Hodei <hmaidaganur20dw@ikzubirimanteo.com>
2022-01-24 08:46:56 +0100

    Logo no enseñaba

--- resources/views/locked.blade.php
+++ resources/views/locked.blade.php
@@ -3,12 +3,12 @@
 
 <head>
     <title>Usuario bloqueado</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('aplicacion/assets/logo_negro.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/logo/logo_negro.ico') }}" />
 </head>
 
 <body>
     <div style="text-align:center;">
-        <img src="{{ asset('aplicacion/assets/logo_negro.ico')}}">
+        <img src="{{ asset('assets/logo/logo_negro.ico')}}">
         <h1>Usuario Bloqueado</h1>
         <h3>Contacte con el administrador</h3>
         <h4>Disculpe las molestias</h4>
